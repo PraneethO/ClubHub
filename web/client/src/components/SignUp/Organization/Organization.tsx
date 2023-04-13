2e483af2a5b9595f7a6dae3b949c6d0467ee5293
Praneeth Otthi <73143434+PraneethO@users.noreply.github.com>
2023-04-13 19:20:58 -0400

    Changed Styling to Work With 100% Zoom

--- web/client/src/components/SignUp/Organization/Organization.tsx
+++ web/client/src/components/SignUp/Organization/Organization.tsx
@@ -1,7 +1,34 @@
-import "./Organization.css";
-
 function Organization() {
-  return <h1>Organization Goes Here</h1>;
+  return (
+    <div className="formContainer">
+      <form className="studentForm">
+        <div className="userEmailBox">
+          <div className="inputBox">
+            <label>USERNAME</label>
+            <br />
+            <input type="text" name="username" />
+          </div>
+          <div className="inputBox">
+            <label>EMAIL</label>
+            <br />
+            <input type="text" name="email" />
+          </div>
+        </div>
+        <div className="inputBox">
+          <label>PASSWORD</label>
+          <br />
+          <input type="password" name="password" />
+        </div>
+
+        <br />
+        <br />
+        <br />
+        <button type="submit" className="signButtonContainer">
+          Sign Up!
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Organization;
