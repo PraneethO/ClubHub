c87ead875baefc0a268310a40e14eb7df40a543f
Praneeth Otthi <73143434+PraneethO@users.noreply.github.com>
2023-04-13 19:44:31 -0400

    I'm gonna add modular css nex time (tomorrow or the weekend)

--- web/client/src/components/SignUp/Student/Student.tsx
+++ web/client/src/components/SignUp/Student/Student.tsx
@@ -5,21 +5,37 @@ function Student() {
     <div className="formContainer">
       <form className="studentForm">
         <div className="userEmailBox">
-          <div className="inputBox">
+          <div className="inputBox username">
             <label>USERNAME</label>
             <br />
             <input type="text" name="username" />
           </div>
-          <div className="inputBox">
+
+          <div className="inputBox email">
             <label>EMAIL</label>
             <br />
             <input type="text" name="email" />
           </div>
         </div>
-        <div className="inputBox">
+
+        <div className="password">
           <label>PASSWORD</label>
           <br />
           <input type="password" name="password" />
+        </div>
+
+        <div className="idGradeBox">
+          <div className="inputBox grade">
+            <label>GRADE</label>
+            <br />
+            <input type="text" name="grade" />
+          </div>
+
+          <div className="inputBox id">
+            <label>STUDENT ID</label>
+            <br />
+            <input type="text" name="id" />
+          </div>
         </div>
 
         <br />
