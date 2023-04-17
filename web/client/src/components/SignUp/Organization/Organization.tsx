b80eb552ba529ddc783db1505bcdc640715a9de0
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-04-17 19:27:29 -0400

    org done but password

--- web/client/src/components/SignUp/Organization/Organization.tsx
+++ web/client/src/components/SignUp/Organization/Organization.tsx
@@ -1,41 +1,62 @@
+import "./Organization.css";
+
 function Organization() {
   return (
     <div className="formContainer">
-      <form className="studentForm">
-        <div className="inputBox">
-          <label>ORGANIZATION NAME</label>
-          <br />
-          <input type="text" name="organizationName" className="inputBoxText" />
-        </div>
-        <div className="idGradeBox">
+      <form className="organizationForm">
+        <div className="nameDesignationBox">
           <div className="inputBox">
-            <label>OFFICIAL DESIGNATION</label>
-            <br />
+            <label>Organization Name</label>
             <input
               type="text"
-              name="officialDesignation"
               className="inputBoxText"
+              autoCapitalize="words"
             />
           </div>
+
           <div className="inputBox">
-            <label>REGION</label>
-            <br />
+            <label>Official Designation</label>
+            <input
+              type="text"
+              className="inputBoxText"
+              autoCapitalize="words"
+            />
+          </div>
+        </div>
+
+        <div className="userEmailBox">
+          <div className="inputBox emailBox">
+            <label>Email</label>
+            <input
+              type="email"
+              className="inputBoxText"
+              style={{ width: "210%" }}
+              autoCapitalize="off"
+            />
+          </div>
+        </div>
+
+        <div className="inputBox password">
+          <label>Password</label>
+          <input type="password" name="password" className="inputBoxText" />
+        </div>
+
+        <div className="idGradeBox">
+          <div className="inputBox field">
+            <label>Field</label>
+            <select name="field" className="inputBoxText">
+              <option value="Computer Science">Computer Science</option>
+              <option value="Medicine">Medicine</option>
+              <option value="Service">Option 3</option>
+            </select>
+          </div>
+
+          <div className="inputBox region">
+            <label>Region</label>
             <input type="text" name="region" className="inputBoxText" />
           </div>
         </div>
-        <div className="userEmailBox">
-          <div className="inputBox">
-            <label>EMAIL</label>
-            <br />
-            <input type="text" name="email" className="inputBoxText" />
-          </div>
-          <div className="inputBox password">
-            <label>PASSWORD</label>
-            <br />
-            <input type="password" name="password" className="inputBoxText" />
-          </div>
-        </div>
-        <br />
+
         <br />
         <br />
         <button type="submit" className="signButtonContainer">
