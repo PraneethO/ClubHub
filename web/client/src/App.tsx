f9003af4c4fa5c066d21044f59a50a9c2574769b
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-04-18 14:13:09 -0400

    vineel

--- web/client/src/App.tsx
+++ web/client/src/App.tsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 
 import LandingPage from "./components/LandingPage/LandingPage";
 import SignUp from "./components/SignUp/SignUp";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/signUp" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
---
+++ web/client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,20 @@
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+    <nav>
+      {/* <Link id="logo" to="/">
+        <div style={{ fontSize: "60px", fontFamily: "Bebas Neue" }}>
+          ClubHub
+        </div>
+      </Link>
+
+      <Link to="/signUp" id="signButtonContainer">
+        {isLoggedIn()}
+      </Link> */}
+      <div style={{ fontSize: "60px", fontFamily: "Bebas Neue" }}>ClubHub</div>
+    </nav>
+  );
+}
+
+export default Dashboard;
