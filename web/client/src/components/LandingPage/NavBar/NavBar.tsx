16aa1d0fb018c98b72a32d56cf59d4e920cacf95
Praneeth Otthi <73143434+PraneethO@users.noreply.github.com>
2023-07-20 16:29:20 -0400

    did stuff

--- web/client/src/components/LandingPage/NavBar/NavBar.tsx
+++ web/client/src/components/LandingPage/NavBar/NavBar.tsx
@@ -2,11 +2,13 @@ import "./NavBar.css";
 import { Link } from "react-router-dom";
 import muffinLogo from "../../../assets/mufkin-logo.png";
 
+import logo from "../../../assets/logo.gif";
+
 function Navbar() {
   return (
     <nav>
       <Link id="logo" to="/">
-        <div>ClubHub</div>
+        <img src={logo} style={{ height: "20rem", width: "25rem" }} />
       </Link>
       <Link to="/sign-up" id="signButtonContainerNav">
         Join the Beta
