dcbd73b77a769d8a0ba74fe0fc80a2de76227ee2
cardinal73 <109763528+cardinal73@users.noreply.github.com>
2023-11-06 14:44:02 -0500

    joe

--- club-hub/src/app/pages/user-profile/page.tsx
+++ club-hub/src/app/pages/user-profile/page.tsx
@@ -9,6 +9,9 @@ import axios from "axios";
 import formatPhoneNumber from "../sign-up/formatPhoneNumber";
 import SearchBar from "../dashboard/navbar";
 
+import { FaExpandArrowsAlt } from 'react-icons/fa';
+import { FaRegArrowAltCircleRight } from 'react-icons/fa';
+
 export default function UserProfile() {
   const router = useRouter();
 
@@ -27,10 +30,20 @@ export default function UserProfile() {
   const [zip, setZip] = useState(0);
 
   const [picture, setPicture] = useState("");
+  const [resume, setResume] = useState("");
+  const [statement, setStatement] = useState("");
 
   const handlePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
     const formatted = formatPhoneNumber(e.target.value);
     setPhoneNumber(formatted);
+  };
+
+  const handleStatementChange = (e: { target: { value: any; }; }) => {
+    const input = e.target.value;
+    const cleanedInput = input.replace(/\s+/g, " ").trim();
+    if (cleanedInput.length <= 500) {
+      setStatement(cleanedInput);
+    }
   };
 
   useEffect(() => {
@@ -285,65 +298,91 @@ export default function UserProfile() {
           <div className={styles.infoContainer}>
             <div className={styles.infoRow}>
               <div className={styles.descriptionContainer}>
-                <div className={styles.description}>First Name</div>
+                <div className={styles.description}>Upload Resume
+                  <div style={{ fontSize: "1rem", marginLeft: "1rem" }}>**only visible by organizations</div>
+                </div>
               </div>
               <input
                 className={styles.infoInput}
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
+                value={resume}
+                onChange={(e) => setResume(e.target.value)}
+                type="file"
               />
             </div>
             <div className={styles.infoRow}>
               <div className={styles.descriptionContainer}>
-                <div className={styles.description}>Last Name</div>
+                <div className={styles.description}>Personal Statement
+                  <div style={{ fontSize: "1rem", marginLeft: "1rem", marginTop: "auto", marginBottom: "auto" }}>(500 character max)</div>
+                </div>
               </div>
-              <input
+              <textarea
                 className={styles.infoInput}
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
+                value={statement}
+                onChange={(e) => handleStatementChange(e)}
+                rows={6}
+                style={{ maxWidth: "100%", maxHeight: "30%", minWidth: "100%", minHeight: "10em" }}
+                placeholder="Start typing..."
               />
-            </div>
-            <div className={styles.infoRow}>
-              <div className={styles.descriptionContainer}>
-                <div className={styles.description}>Email</div>
-              </div>
-              <input
-                className={styles.infoInput}
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className={styles.infoRow}>
-              <div className={styles.descriptionContainer}>
-                <div className={styles.description}>Phone Number</div>
-              </div>
-              <input
-                className={styles.infoInput}
-                value={phoneNumber}
-              // onChange={(e) => handlePhoneNumber(e)}
-              />
-            </div>
-            <div className={styles.infoRow}>
-              <div className={styles.descriptionContainer}>
-                <div className={styles.description}>Grade</div>
-              </div>
-              <input
-                className={styles.infoInput}
-                value={grade}
-              // onChange={(e) => setGrade(parseInt(e.target.value, 10))}
-              />
-            </div>
-
-            <div className={styles.infoRow}>
-              <div className={styles.descriptionContainer}>
-                <div className={styles.description}>Birth Date</div>
-              </div>
-              <input className={styles.infoInput} type="date" />
             </div>
           </div>
         </div>
-        <button className={styles.updateButton}>Update Changes</button>
+
+
+        <div className={styles.fieldChangeContainer}>
+          <div className={styles.fieldChangeTitle}>Employment History</div>
+          <div className={styles.infoContainer}>
+            <div className={styles.infoRow}>
+              <div className={styles.descriptionContainer}>
+                <div className={styles.description}>Organization</div>
+              </div>
+              <input
+                className={styles.infoInput}
+                value={school}
+                onChange={(e) => setSchool(e.target.value)}
+              />
+            </div>
+            <div className={styles.infoRow}>
+              <div className={styles.descriptionContainer}>
+                <div className={styles.description}>Position</div>
+              </div>
+              <input
+                className={styles.infoInput}
+                value={school}
+                onChange={(e) => setSchool(e.target.value)}
+              />
+            </div>
+            <div className={styles.infoRow}>
+              <div className={styles.descriptionContainer}>
+                <div className={styles.firstHalfDescription}>Start Date</div>
+                <div className={styles.lastHalfDescription}>
+                  End Date
+                  <input type="checkbox" style={{ marginLeft: "1rem" }}></input>
+                  <div style={{ fontSize: "1rem", marginTop: "auto", marginBottom: "auto", textDecoration: "underline" }}>Currently Working</div>
+                </div>
+              </div>
+              <div className={styles.splitInputContainer}>
+                <input className={styles.splitInput} type="date" />
+                <input className={styles.splitInput} type="date" />
+              </div>
+            </div>
+          </div>
+          <div className={styles.infoContainer}>
+            <button className={styles.updateButton} style={{ marginTop: "0", marginBottom: "0", width: "100%" }}>Add Position</button>
+          </div>
+        </div>
+
+
+        <button className={styles.updateButton} style={{ borderRadius: "2rem", width: "fit-content", marginLeft: "auto", marginRight: "10%" }}>
+          <div style={{ marginTop: "auto", marginBottom: "auto", fontFamily: "bebas-neue-pro, sans-serif" }}>Update All Changes</div>
+          <FaRegArrowAltCircleRight style={{ marginLeft: "0.5rem", marginTop: "auto", marginBottom: "auto", fontSize: "1.5rem" }} />
+        </button>
       </div>
+
+      {/* <button className={styles.updateButton} style={{ borderRadius: "2rem", width: "fit-content", marginLeft: "auto", marginRight: "10%", backgroundColor: "white" }}>
+        <div style={{ marginTop: "auto", marginBottom: "auto", fontFamily: "bebas-neue-pro, sans-serif", color: "#044e8b" }}>Update All Changes</div>
+        <FaRegArrowAltCircleRight style={{ marginLeft: "0.5rem", marginTop: "auto", marginBottom: "auto", fontSize: "1.5rem", color: "#044e8b" }} />
+      </button> */}
+      
     </main>
   );
 }
